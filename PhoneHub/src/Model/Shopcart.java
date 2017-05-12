2901eb35b171300db132d3e4f544b123b7d47684
fevenBaby <18814379641@163.com>
2017-05-13 07:52:10 +0800

    add Shopcart class under the Model package modify Order class

---
+++ PhoneHub/src/Model/Shopcart.java
@@ -0,0 +1,35 @@
+package Model;
+
+public class Shopcart {
+
+	private int userId;
+	private int productId;
+	private int num;
+	
+	
+	
+	public Shopcart() {
+		super();
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
+	
+}
